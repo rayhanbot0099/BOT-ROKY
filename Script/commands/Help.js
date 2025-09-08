6aca5f539d775dcf967a5ba1a0c8e5cc341f2adf
RIYA-ROKY-009 <roky27139@gmail.com>
2025-09-08 08:03:16 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -32,7 +32,7 @@ module.exports.languages = {
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: %8
 ┃ 🤖 Bot Name: %9
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 𝐑𝐀𝐈𝐇𝐀𝐍 𝐊𝐇𝐀𝐍
 ╰━━━━━━━━━━━━━━━━╯`,
         "helpList": "[ There are %1 commands. Use: \"%2help commandName\" to view more. ]",
         "user": "User",
@@ -44,10 +44,10 @@ module.exports.languages = {
 // এখানে আপনার ফোটো Imgur লিংক করে বসাবেন✅
 
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/sxSn1K3.jpeg"
+    "https://i.imgur.com/FX8CnQo.jpeg",
+    "https://i.imgur.com/FX8CnQo.jpeg",
+    "https://i.imgur.com/FX8CnQo.jpeg",
+    "https://i.imgur.com/FX8CnQo.jpeg"
 ];
 
 function downloadImages(callback) {
@@ -115,7 +115,7 @@ module.exports.run = function ({ api, event, args, getText }) {
             command.config.commandCategory || "Unknown",  
             command.config.cooldowns || 0,  
             prefix,  
-            global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"  
+            global.config.BOTNAME || "❖•÷[❀𝑹𝑨𝑰𝑯𝑨𝑵❀✿𝐁𝐎𝐓❀]÷•❖"  
         );  
 
         downloadImages(files => {  
@@ -149,7 +149,7 @@ ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
 ┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 𝐑𝐀𝐈𝐇𝐀𝐍 𝐊𝐇𝐀𝐍
 ╰━━━━━━━━━━━━━━━━╯`;
 
     downloadImages(files => {  
